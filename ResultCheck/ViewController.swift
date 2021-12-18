8ce29d388ff2881a8fee80b067f1bc1fe8771f9e
AoiKiryu <aoaogoma0204@gmail.com>
2021-12-18 20:06:56 +0900

    first commit

---
+++ ResultCheck/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ResultCheck
+//
+//  Created by 桐生あおい新 on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
+
